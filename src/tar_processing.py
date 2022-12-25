7d70da41065113c1bfc48a6f0049a67fcb7cd5e2
halt <halt9000@protonmail.com>
2022-12-26 01:40:06 +0300

    wip: file content extraction working

---
+++ src/tar_processing.py
@@ -0,0 +1,29 @@
+import os
+import tarfile
+
+def unpack_tar(arc_path):
+
+    mode = "r:"
+    if arc_path.endswith("tar.gz"):
+        mode = "r:gz"
+
+    f = open(arc_path, 'rb')
+    tar = tarfile.open(fileobj=f, mode=mode)
+    # for item in tar:
+    return tar
+
+
+def process_arcive(arc_path, filter_extensions):
+    tar = unpack_tar(arc_path)
+
+    extracted_data = {}
+    for item in tar:
+        fname = os.path.basename(item.name)
+
+        if filter_extensions:
+            if not os.path.splitext(fname)[1] in filter_extensions:
+                continue
+
+        extracted_data[fname] = tar.extractfile(item.path).read()
+        print(item)
+    return extracted_data
