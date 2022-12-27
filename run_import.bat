1e97e7c24b31dd0f480b72aaae59d446978821f6
halt9k <49090368+halt9k@users.noreply.github.com>
2022-12-27 21:11:36 +0300

    fix: execution via 3 ways

--- run_import.bat
+++ run_import.bat
@@ -1,17 +1,46 @@
-REM Check for Python Installation
+cls
 
-echo off
+REM @echo off
 set PYTHON_DIR="C:\ProgramData\Anaconda3\envs\temp\"
-%PYTHON_DIR%\python.exe --version 2>NUL
+REM set ENV_DIR="C:\ProgramData\Anaconda3\Scripts\"
 
-if errorlevel 1 (
-	echo Error^: Python not installed
-	goto:exit
-	)
+IF [%ENV_DIR%] == [] (
+	goto try_python
+)
+if exist %ENV_DIR% (
+	echo "Run via environment"
 
-REM OpenSSL fix for Anacodna
-SET PATH=%PATH%;%PYTHON_DIR%\Library\bin\
-%PYTHON_DIR%\python.exe src\main.py
+	REM OpenSSL fix for Anacodna
+	REM SET PATH=%PATH%;%PYTHON_DIR%\Library\bin\
+	
+	%ENV_DIR%\conda.exe run -n base python src\main.py
+	
+	echo "Run via environment done"
+	goto exit	
+)
+
+
+:try_python
+IF [%PYTHON_DIR%] == [] goto try_raw
+
+if exist %PYTHON_DIR% (
+	echo "Run via Python Installation"
+	
+	%PYTHON_DIR%\python.exe --version 2>NUL
+
+	if errorlevel 1 (
+		echo Error^: Python not installed
+		goto:exit
+		)
+		
+	%PYTHON_DIR%\python.exe src\main.py
+	echo "Run via Python Installation done"
+	goto exit
+)
+
+:try_raw
+echo "Run via bare python"
+python.exe src\main.py
 
 :exit
 set /p DUMMY=Hit ENTER to continue...
