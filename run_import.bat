c9912b4a5e606287fb0f2da836c20e99348f265b
halt9k <49090368+halt9k@users.noreply.github.com>
2022-12-28 02:37:21 +0300

    feat: embedded version almost fluent except tkinter

--- run_import.bat
+++ run_import.bat
@@ -1,5 +1,6 @@
 cls
 
-.\python-3.11.1-embed-amd64\python main.py
+REM ..\..\..\LOCAL\Dev\python-3.11.1-embed-amd64\python main.py
+python-3.11.1-embed-amd64\python main.py
 
 set /p DUMMY=Hit ENTER to continue...
