4d25ac4fc8e7d2494fb8e2c1297a08557eb19875
halt <halt9000@protonmail.com>
2022-12-29 04:41:35 +0300

    feat: embedded install works, but not tkinter install

--- install_python.ps1
+++ install_python.ps1
@@ -1,5 +1,14 @@
 Cls
-cd python
+
+$PYHTON_PATH = '.\python'
+if (Test-Path -Path $PYHTON_PATH) {
+    Write-Output "Python folder already exists. To try reinstall, delete folder or modify ps1"
+    Exit
+} else {
+    New-Item -ItemType Directory -Force -Path $PYHTON_PATH
+    cd $PYHTON_PATH
+}
+
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 
 wget https://www.python.org/ftp/python/3.9.10/python-3.9.10-embed-amd64.zip -o python-3.9.10-embed-amd64.zip
---
+++ run_import.ps1
@@ -0,0 +1,27 @@
+Cls
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+
+$ENV_PATH = '.\python\testenv\Scripts\Activate.ps1'
+$DEFAULT_ENV_PATH = './env/Scripts/activate.ps1'
+if (Test-Path -Path $ENV_PATH) {
+    Write-Output "Trying to run under portable enviroment"
+    $ENV_PATH    
+} elseif (Test-Path -Path $DEFAULT_ENV_PATH) {
+    Write-Output "Trying to run under default Windows python enviroment"
+    ./env/Scripts/activate.ps1    
+} else {
+    Write-Output "No script enviroments found. Fix manually based on this script source."
+    Exit
+}
+
+$py_version = (&{python -V}).Exception.Message
+if ($py_version){
+    Write-Output "Python version is missing or lower than 3. Something is broken.", $py_version
+    Exit
+}
+
+
+python src\main.py
+
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
