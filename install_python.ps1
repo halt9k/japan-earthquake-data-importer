f06e47b6a0e1510ad78909111427294c0def204e
halt9k <49090368+halt9k@users.noreply.github.com>
2022-12-29 06:13:58 +0300

    wip: all works

--- install_python.ps1
+++ install_python.ps1
@@ -11,17 +11,32 @@ if (Test-Path -Path $PYHTON_PATH) {
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 
-wget https://www.python.org/ftp/python/3.9.10/python-3.9.10-embed-amd64.zip -o python-3.9.10-embed-amd64.zip
-Expand-Archive python-3.9.10-embed-amd64.zip
-wget https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-mv python-3.9.10-embed-amd64\python39._pth python-3.9.10-embed-amd64\python39.pth
-mkdir python-3.9.10-embed-amd64\DLLs
-python-3.9.10-embed-amd64\python.exe get-pip.py
+function Install-Embedded-Python {
+	wget https://www.python.org/ftp/python/3.9.10/python-3.9.10-embed-amd64.zip -o python-3.9.10-embed-amd64.zip
+	Expand-Archive python-3.9.10-embed-amd64.zip
+	wget https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+	mv python-3.9.10-embed-amd64\python39._pth python-3.9.10-embed-amd64\python39.pth
+	mkdir python-3.9.10-embed-amd64\DLLs
+	python-3.9.10-embed-amd64\python.exe get-pip.py
+	 
+	python-3.9.10-embed-amd64\python.exe -m pip install virtualenv
 
-python-3.9.10-embed-amd64\python.exe -m pip install virtualenv
+	python-3.9.10-embed-amd64\python.exe -m virtualenv testenv
+	cp python-3.9.10-embed-amd64\python39.zip testenv\Scripts\
+}
 
-python-3.9.10-embed-amd64\python.exe -m virtualenv testenv
-cp python-3.9.10-embed-amd64\python39.zip testenv\Scripts\
+
+function Install-Silent-Python {
+	wget https://www.python.org/ftp/python/3.11.1/python-3.11.1-amd64.exe -o python-3.11.1-amd64.exe
+    $Target = Resolve-Path .
+    $Target = Join-Path -Path $Target -Childpath 'python-3.11.1-amd64'
+	.\python-3.11.1-amd64.exe InstallAllUsers=1 TargetDir=$Target AssociateFiles=0 InstallLauncherAllUsers=0 /passive
+
+	python-3.11.1-amd64\python.exe -m pip install virtualenv
+
+	python-3.11.1-amd64\python.exe -m virtualenv testenv	
+}
+Install-Silent-Python
 
 testenv\Scripts\Activate.ps1
 
