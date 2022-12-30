d303ba082659bf4bc2cd5133425ee474a56f4229
halt <halt9000@protonmail.com>
2022-12-30 20:36:43 +0300

    fix: powershell tweaks

--- run_import.ps1
+++ run_import.ps1
@@ -4,11 +4,11 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 $ENV_PATH = '.\python\testenv\Scripts\Activate.ps1'
 $DEFAULT_ENV_PATH = './env/Scripts/activate.ps1'
 if (Test-Path -Path $ENV_PATH) {
-    Write-Output "Trying to run under portable enviroment"
-    $ENV_PATH    
+	Write-Output "Trying to run under portable enviroment"
+	& $ENV_PATH
 } elseif (Test-Path -Path $DEFAULT_ENV_PATH) {
     Write-Output "Trying to run under default Windows python enviroment"
-    ./env/Scripts/activate.ps1    
+    & $DEFAULT_ENV_PATH
 } else {
     Write-Output "No script enviroments found. Fix manually based on this script source."
     Exit
