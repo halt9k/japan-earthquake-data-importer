0ddf05b4373d809df146abb11902991bf91c9a09
halt9k <49090368+halt9k@users.noreply.github.com>
2023-04-19 19:42:12 +0300

    wip: raw merge done

--- src/config.py
+++ src/config.py
@@ -1,4 +1,12 @@
 from configparser import ConfigParser, NoOptionError
+from enum import Enum
+
+
+class ImportModes:
+    MULTIPLE_PAGES = 'multiple_pages'
+    SINGLE_PAGE = 'single_page'
+    ALL = [MULTIPLE_PAGES, SINGLE_PAGE]
+
 
 DEFAULT_CONFIG = {
     'Data sources': {
@@ -6,6 +14,9 @@ DEFAULT_CONFIG = {
         'default_arc_paths': [],
         'ask_xls_template': False,
         'default_xls_template': ''
+    },
+    'Excel import': {
+        'import_mode': ImportModes.MULTIPLE_PAGES
     },
     'UX': {
         'slow_paced_import': True
@@ -35,11 +46,12 @@ class AppConfig:
             opts += list(parser.options(s))
         return opts
 
-    def read_config(self, conf_path):
-        ini_config = ConfigParser(strict=True)
-        ini_config.read(conf_path)
-        default_config = AppConfig.get_default_config()
+    @staticmethod
+    def validate_enums(config):
+        assert(config['Excel import']['import_mode'] in ImportModes.ALL)
 
+    @staticmethod
+    def ensure_correct_config_file(default_config, ini_config, conf_path):
         deafult_opts = AppConfig.all_options(default_config)
         ini_file_opts = AppConfig.all_options(ini_config)
 
@@ -47,6 +59,15 @@ class AppConfig:
         old_opts = set(deafult_opts) - set(ini_file_opts)
         if len(new_opts) > 0:
             raise NoOptionError(str(new_opts) + '; probably intended : ' + str(old_opts), section=conf_path)
+
+        AppConfig.validate_enums(ini_config)
+
+    def read_config(self, conf_path):
+        ini_config = ConfigParser(strict=True)
+        ini_config.read(conf_path)
+
+        default_config = AppConfig.get_default_config()
+        AppConfig.ensure_correct_config_file(default_config, ini_config, conf_path)
 
         # all checked, read again to do override of defaults
         self.__config = default_config
