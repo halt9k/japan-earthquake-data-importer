54b34e1afa282264ad9d0780f64229d810df6222
halt9k <49090368+halt9k@users.noreply.github.com>
2023-08-21 04:15:18 +0300

    feat: backup of very raw side-task batch conversion used once

--- src/main.py
+++ src/main.py
@@ -1,124 +1,100 @@
+import csv
 import glob
-import os
-import subprocess
+from pathlib import Path
 
-# TODO answer SO questions on deploy error if this line missing
-
-from config import APP, ImportModes
-from errors import err_exit, debugger_is_active
-from jap_arc_parser import jap_arcs_to_xlsx, verify_file_exists
-from tkinter import filedialog
-
-CONFIG_FILE = 'settings.ini'
-ARC_EXT = '.tar.gz'
-ARC_MASK = '*' + ARC_EXT
-XLS_EXT = '.xlsx'
-XLS_MASK = '*' + XLS_EXT
-
-
-# overriden during tests
-def os_view_path(path):
-    # subprocess.Popen(r'explorer /select,"' + name + '"')
-    dir_path = os.path.abspath(path)
-    full_path = r'explorer /select,"' + dir_path + '"'
-    subprocess.Popen(full_path)
-
-
-def get_valid_paths(try_paths, is_silent, ask_filetypes, ask_title, allow_multiple):
-    if is_silent:
-        for path in try_paths:
-            verify_file_exists(path)
-        return try_paths
-
-    closest_valid_dir = os.path.commonpath(try_paths)
-    if not os.path.isdir(closest_valid_dir):
-        closest_valid_dir = os.path.dirname(closest_valid_dir)
-
-    try_files = ' '.join(['"{}"'.format(os.path.basename(x)) for x in try_paths])
-
-    paths = filedialog.askopenfilename(filetypes=ask_filetypes,
-                                       initialdir=closest_valid_dir, initialfile=try_files,
-                                       title=ask_title, multiple=allow_multiple)
-    if not paths:
-        err_exit('canceled')
-
-    if allow_multiple and type(paths) is tuple:
-        return list(paths)
-    elif not allow_multiple and type(paths) is str:
-        return [paths]
-    else:
-        assert False
-
-
-def get_config_path(try_path, is_silent, ask_filetypes, ask_title):
-    if try_path:
-        assert type(try_path) is str
-    res = get_valid_paths([try_path], is_silent, ask_filetypes, ask_title, allow_multiple=False)
-    if res:
-        assert type(res) is list
-    return res[0] if res else None
-
-
-def get_config_paths(try_paths, is_silent, ask_filetypes, ask_title):
-    if try_paths:
-        assert type(try_paths) is list
-    res = get_valid_paths(try_paths, is_silent, ask_filetypes, ask_title, allow_multiple=True)
-    if res:
-        assert type(res) is list
-    return res
+import regex
+import pandas as pd
+import win32com.client as win32
+from xlsxwriter import Workbook
 
 
 def get_archives(arc_path):
-    path_mask = os.path.join(arc_path, ARC_MASK)
-    arc_paths = glob.glob(path_mask)
-
-    arcives_count = len(arc_paths)
-    if not 2 <= arcives_count <= 10:
-        err_exit('amount of earthquake arcives unexpected: ' + str(arcives_count))
+    arc_paths = glob.glob(arc_path, recursive=True)
 
     return arc_paths
 
 
-# TODO separate into additional processing
-def import_data(try_data_arc_paths, ask_data_paths, try_template_path, ask_tamplate_path):
-    arc_paths = get_config_paths(try_data_arc_paths, not ask_data_paths, [('Japan archives', ARC_MASK)],
-                                 'Select source archive')
-
-    if type(arc_paths) is tuple and len(arc_paths) == 1:
-        arc_info = os.path.basename(arc_paths[0])
-    else:
-        arc_info = ' count of ' + str(len(arc_paths))
-    title = 'Select excel template for source archive ' + arc_info
-    template_path = get_config_path(try_template_path, not ask_tamplate_path, [('Excel files', XLS_MASK)], title)
-
-    if len(arc_paths) == 1 and os.path.isdir(arc_paths[0]):
-        arc_paths = get_archives(arc_paths[0])
-
-    skip_data = APP.config()['Excel import']['import_mode'] == ImportModes.SINGLE_PAGE
-    # TODO improve config handling
-    xlsx_fname = jap_arcs_to_xlsx(arc_paths, template_path, skip_data, APP.config())
-
-    return xlsx_fname
+def get_containing_word(words, text):
+    matches = [x for x in words if x in text]
+    assert (len(matches) == 1)
+    return matches[0]
 
 
-def process_dir():
-    ask_data_paths = APP.config()['Data sources'].getboolean('ask_archive')
-    try_data_paths = APP.get_list(APP.config(), 'Data sources', 'default_arc_paths')
+def add_year(path):
+    parent_dirs = ['Logger', 'Ground', 'Eddy']
+    match = get_containing_word(parent_dirs, path)
+    regex_match = match + '\\\\((19|20)\\d{2})'
 
-    try_template_path = APP.config()['Data sources']['default_xls_template']
-    ask_template = APP.config()['Data sources'].getboolean('ask_xls_template')
+    re_match = regex.search(regex_match, path)
+    assert (re_match)
+    year = re_match[1]
 
-    xlsx_fname = import_data(try_data_paths, ask_data_paths, try_template_path, ask_template)
+    old_path = Path(path)
+    new_name = year + '_' + old_path.name
+    new_path = old_path.parent.joinpath(new_name)
 
-    if xlsx_fname and debugger_is_active():
-        os_view_path(xlsx_fname)
+    return str(new_path)
 
-    return
+
+def convert_xls_to_xlsx(xls_path, xlsx_path):
+    excel = win32.gencache.EnsureDispatch('Excel.Application')
+    wb = excel.Workbooks.Open(xls_path)
+
+    # FileFormat = 51 is for .xlsx extension
+    # FileFormat = 56 is for .xls extension
+    wb.SaveAs(xlsx_path, FileFormat=51)
+
+    wb.Close()
+    excel.Application.Quit()
+
+
+def convert_csv_to_xlsx(csv_path, xlsx_path):
+    workbook = Workbook(xlsx_path, {'strings_to_numbers': True, 'constant_memory': True})
+    worksheet = workbook.add_worksheet()
+    with open(csv_path, 'rt', encoding='utf8') as f:
+        reader = csv.reader(f)
+        for r, row in enumerate(reader):
+            for c, col in enumerate(row):
+                worksheet.write(r, c, col)
+    workbook.close()
 
 
 def main():
-    APP.read_config(CONFIG_FILE)
-    process_dir()
+    xls_files = get_archives('**/*.xls')
+    csv_files = get_archives('**/*.csv')
+
+    src_files = xls_files + csv_files
+    conversion_errors = []
+
+    for i, src_rel_file in enumerate(src_files):
+        path = Path(src_rel_file)
+        fixed_path = Path(path.parent).joinpath(path.name.lower())
+
+        ext = fixed_path.suffix
+        src_file = str(fixed_path.absolute())
+
+        match = get_containing_word(['.xls', '.csv'], src_file)
+        out_name = add_year(src_file).replace(match, '.xlsx')
+
+        if Path(out_name).exists():
+            print('Already converted, skipping: ' + src_file + ' -> ' + str(out_name))
+            continue
+
+        try:
+            if match == '.xls':
+                convert_xls_to_xlsx(src_file, out_name)
+            elif match == '.csv':
+                convert_csv_to_xlsx(src_file, out_name)
+            else:
+                assert False
+        except:
+            conversion_errors += [src_file]
+            continue
+
+        print('Done: {0} of {1}  {2} -> {3} '.format(i, len(src_files), src_file, out_name))
+
+    if len(conversion_errors) > 0:
+        print('\n ERRORS HAPPENED ON FILES: \n' + '\n'.join(conversion_errors))
 
 
 if __name__ == '__main__':
